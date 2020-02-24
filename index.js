3b4d80e2db8d37a215a48dd4f1797faa365f93fb
Jonathan East <jonathan_east@ymail.com>
2020-02-24 14:48:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  var newKittens = Array.push(name);
+  var newArray = [];
+  var newKittens = newArray.push(name);
   return newKittens;
 }
